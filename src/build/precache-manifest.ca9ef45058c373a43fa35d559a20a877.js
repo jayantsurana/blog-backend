cc8c5982a1978c3adc7ad6c79565bf189b4b0d5a
jayantsurana <jayantplb@gmail.com>
2019-10-03 21:11:41 +0530

    Added Front-end Build

---
+++ src/build/precache-manifest.ca9ef45058c373a43fa35d559a20a877.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe2d19dc5a1494de2d1bc8434fc20d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a065958105fd01083f5b",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "c1c13bea20e74d934307",
+    "url": "/static/js/2.c697d125.chunk.js"
+  },
+  {
+    "revision": "a065958105fd01083f5b",
+    "url": "/static/js/main.2f4c6d69.chunk.js"
+  },
+  {
+    "revision": "afc0a052c644dce5e6bc",
+    "url": "/static/js/runtime-main.fbd335c9.js"
+  }
+]);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
-
+import path from 'path';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(bodyParser.json());
 
 const withDB = async (operations, res) => {
@@ -74,5 +75,9 @@ app.post('/api/articles/:name/add-comment', async (req, res) => {
 // app.get('/hello', (req, res) => res.send('Hello!'));
 // app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}`));
 // app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}!`));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 app.listen(8000, () => console.log('Listening on port 8000'));
